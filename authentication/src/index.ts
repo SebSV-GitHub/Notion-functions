ef8789820b6b92fb22b7701513af83012b7c32f5
Sebastián Suárez Valencia <102331372+SebSV-GitHub@users.noreply.github.com>
2025-05-16 13:26:13 -0500

    Fix Authentication process

--- authentication/src/index.ts
+++ authentication/src/index.ts
@@ -1,16 +1,32 @@
+import net from "node:net";
 import type {
 	APIGatewayRequestAuthorizerEventV2,
 	APIGatewaySimpleAuthorizerResult,
 } from "aws-lambda";
+import IPWhitelist from "../ip-whitelist.json";
 
-function handler(
+async function handler(
 	event: APIGatewayRequestAuthorizerEventV2
-): APIGatewaySimpleAuthorizerResult {
-	console.log(event);
+): Promise<APIGatewaySimpleAuthorizerResult> {
+	console.log("[Authorizer]: Validating source IP");
 
-	return {
-		isAuthorized: true,
-	};
+	const { sourceIp } = event.requestContext.http;
+
+	const ipFamily = net.isIP(sourceIp);
+	if (!(ipFamily === 4 || ipFamily === 6)) {
+		return {
+			isAuthorized: false,
+		};
+	}
+
+	const blocklist = new net.BlockList();
+
+	for (const subnet of IPWhitelist.v4) {
+		const [address, prefix] = subnet.split("/");
+		blocklist.addSubnet(address, Number(prefix), "ipv4");
+	}
+
+	return { isAuthorized: blocklist.check(sourceIp) };
 }
 
 export { handler };
